be52f23c3a903d4e6295e99b33f9f57a00d0321d
jitendravaid99 <114829231+jitendravaid99@users.noreply.github.com>
2024-06-22 19:15:21 +0000

    conversions

---
+++ basics/03_conversionOperations.js
@@ -0,0 +1,24 @@
+// let score = "33abc"
+
+// console.log(typeof score)
+
+// let valueInNumber = Number(score) // convert value into number type
+// console.log(typeof valueInNumber) // 
+
+// console.log(valueInNumber) // value is NaN so this can be an issue
+
+// // "33" => 33
+// // true => 1
+
+// let isloggedIn = 1
+
+// let booleanIsloggedIn = Boolean(isloggedIn)
+// console.log(booleanIsloggedIn) // value is NaN so this can be an issue
+
+// 1 convert to ture , 0 convert to false
+
+// "jiten" => true
+
+let someNumber = 33
+let stringNum = String(someNumber)
+console.log(typeof stringNum)

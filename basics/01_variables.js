b9dc7f19b9f67223f9bb8ae07be0b4ecb33b691b
jitendravaid99 <114829231+jitendravaid99@users.noreply.github.com>
2024-06-22 19:03:18 +0000

    variables

---
+++ basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144552
+let accountEmail = "jitendravaid@google.com"
+// var accountPassword = "12345" // don't use var bcause of issue in block scope and functional scope
+accountCity = "Delhi"
+let accountState;
+// accountId = 2 // now allowed
+
+accountEmail = "jitendrav@google.com"
+accountPassword = "1212212"
+accountCity = "bangaluru"
+
+
+// console.log(accountId)
+
+console.table([accountId, accountEmail,accountPassword,accountCity, accountState])
+
---
+++ basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert ("hello")  // we are using node js not browser
+
+let name = "jiten"
+let age = 40
+let isLoggedIn = false
+
+/*
+
+Premitive datatypes
+number => 2 to the power 53
+bigint
+string
+boolean
+null => standalone value
+undefined => not defined
+symbol => unique
+
+
+object 
+ */
+
+console.log(typeof null) // object type
+
+console.log(typeof undefined) // object type
+

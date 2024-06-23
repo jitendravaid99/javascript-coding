5743b4057b837e0b47440a62cf9600e98e709255
jitendravaid99 <114829231+jitendravaid99@users.noreply.github.com>
2024-06-23 12:23:32 +0000

    upto array commit

---
+++ 01_basics/07_dateTime.js
@@ -0,0 +1,28 @@
+// Dates
+
+let myDate = new Date()
+
+// // console.log(myDate);
+// console.log("1", myDate.toString());
+
+
+// console.log("2",myDate.toDateString());
+
+// console.log("3",myDate.toISOString());
+
+// console.log("4", myDate.toTimeString());
+
+// console.log("5", myDate.toUTCString());
+
+// console.log("6", myDate.toLocaleString());
+
+// let myNewDate = new Date(2023, 0, 23)
+
+// // console.log(myNewDate.toLocaleString())
+
+// let myTimeStamp = Date.now()
+
+// console.log(Math.floor(Date.now()/1000))
+
+// // console.log(myNewDate.getTime());
+

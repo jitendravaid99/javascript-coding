4433fb79539804c1cc322302c243e6c0e2880c27
jitendravaid99 <114829231+jitendravaid99@users.noreply.github.com>
2024-06-23 19:31:30 +0000

    commiting scope

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,71 @@
+
+
+
+
+// function sayMyName(){
+//     console.log("J");
+// console.log("I");
+// console.log("T");
+// console.log("E");
+// console.log("N");
+// }
+
+// // sayMyName()
+
+// function addTwo(num1, num2){ // parameters when defining function
+//     console.log(num1 + num2)  
+// }
+
+// addTwo(3,5 ) // arguments -- when calling function
+
+
+// function addTwo(num1, num2){ // parameters when defining function
+//     // let result = num1 + num2
+//     // return result
+//     return num1 + num2
+// }
+
+
+// const result = addTwo(3,6)
+
+// console.log(result)
+
+
+// function loginUserMessage (username= "admin"){
+//     if(!username){
+//         console.log("please enter username")
+//         return
+
+//     }
+//     return `${username} just logged in`
+// }
+
+// console.log(loginUserMessage("jiten"))
+// console.log(loginUserMessage())
+
+// ... is a rest operator here
+function calculateCartPrice(... num1) {
+    return num1
+
+}
+
+console.log(calculateCartPrice(200,300,400))
+
+
+// create user object
+
+const user = {
+    userName: "jiten",
+    price: 999,
+    qty: 1
+}
+
+
+// create function and put user parameter
+
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.userName} and price is ${anyObject.price}`);
+}
+
+handleObject(user)
---
+++ 03_basics/02_scope.js
@@ -0,0 +1,39 @@
+// let a = 300
+
+// if (true){
+//     let a = 10
+//     console.log("INNER", a)
+
+// }
+
+// console.log("OUTER", a)
+
+// function one(){
+//     const username = "jiten"
+
+//     function two(){
+//         const website = "youtube"
+//         console.log(username);
+//     }
+//     // console.log(website);
+//     two()
+// }
+
+// one()
+
+
+// ************************ interesting
+
+
+console.log(addone(5))
+function addone(num){
+    return num +1
+}
+
+addone(5)
+
+const addTwo = function(num){
+    return num +2
+}
+
+addTwo(5)
